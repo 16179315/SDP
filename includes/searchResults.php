e586d353134496938af7eb6cc8c223889795e01c
John Long <john.chris.long@gmail.com>
2018-03-21 13:46:06 +0000

    Search using input search form works only for user names right now

--- includes/searchResults.php
+++ includes/searchResults.php
@@ -8,13 +8,16 @@
             $name = "sql11225471";
 
             $conn = mysqli_connect($serverName, $username, $password, $name);
-            $searchInput = mysqli_real_escape_string($conn, $_POST['search']);
+            $searchInput = mysqli_real_escape_string($conn, $_POST["data"]);
 
-            $users = mysqli_query($conn, "SELECT uFirstName FROM users WHERE uFirstName LIKE '%$searchInput%';");
+            $users = mysqli_query($conn, "SELECT uFirstName FROM users 
+                                                    WHERE uFirstName LIKE" . "'%"."$searchInput"."%';");
 
             while($row = mysqli_fetch_row($users))
             {
                 printf("<ul>%s\n</ul>", $row[0]);
             }
+            var_dump($searchInput);
+            echo $searchInput;
         }
     ?>
