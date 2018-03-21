729a8cc6c541d6827a371184123efaa8d1057d26
John Long <john.chris.long@gmail.com>
2018-03-21 13:27:40 +0000

    Added user search capability to search for users names but not working

---
+++ includes/searchResults.php
@@ -0,0 +1,20 @@
+<?php
+    include 'navbar_template.html';
+
+      if(isset($_POST["search"])){
+            $serverName = "sql11.freemysqlhosting.net";
+            $username = "sql11225471";
+            $password = "cbgPE8apID";
+            $name = "sql11225471";
+
+            $conn = mysqli_connect($serverName, $username, $password, $name);
+            $searchInput = mysqli_real_escape_string($conn, $_POST['search']);
+
+            $users = mysqli_query($conn, "SELECT uFirstName FROM users WHERE uFirstName LIKE '%$searchInput%';");
+
+            while($row = mysqli_fetch_row($users))
+            {
+                printf("<ul>%s\n</ul>", $row[0]);
+            }
+        }
+    ?>
