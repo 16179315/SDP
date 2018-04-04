a089acc93725cd1fe66566e474f3cb396848b909
dairelavelle1 <16192249@studentmail.ul.ie>
2018-04-04 15:43:09 +0100

    Changes to Profile.php

---
+++ EditProfile.php
@@ -0,0 +1,115 @@
+<?php
+  session_start();
+  include 'includes/db.php';
+
+  if($_GET['del']) {
+    $delete_skill_sql="DELETE FROM userSkills WHERE uId=".$_SESSION['uId']." AND sId=".$deleteSkillId;
+    $result=mysql_query($delete_skill_sql);
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Edit Profile</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <link href="style.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script type="text/javascript">
+    function Delete(id) {
+      window.location="EditProfile.php?delSkill="+id;
+    }
+  </script>
+</head>
+<body>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <a class="navbar-brand abs" href="../index.php">HoteledIn</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse collapse" id="collapsingNavbar">
+          <ul class="navbar-nav ml-auto">
+  			<li class="nav-item">
+  					<a class="nav-link" href="includes/logout.php">Log Out</a>
+              </li>
+          </ul>
+      </div>
+  </nav>
+    
+  <div class="container">
+   <div class="row">
+    <div class="col-md-7">
+      <div class="form-group">
+          <label>Upload Image</label>
+          <div class="input-group">
+              <span class="input-group-btn">
+                  <span class="btn btn-default btn-file">
+                      Browse… <input type="file" id="imgInp">
+                  </span>
+              </span>
+              <input type="text" class="form-control" readonly>
+          </div>
+          <img id='img-upload'/>
+      	</div>
+  	</div>
+  	
+  	</div>
+
+
+    <div>
+      <div class="col-md-4">
+    		<div class="form-group">
+    			<label for="comment">Skills:</label>
+          <?php
+          $skill_sql="SELECT sTitle, sId FROM skills s, userSkills u WHERE u\.sId=s\.sId AND u\.uId= '".$_SESSION['uId']."';";
+          $skill_result=$conn->query($skill_sql);
+          if ($conn && ($skill_result->num_rows>0)) {
+              while ($skill_row=$skill_result->fetch_assoc()) {
+                echo "<div class=form-group>
+                        <p>".$skill_row['sTitle']."</p>
+                        <input type=\"button\" name=\"del-skill\" id=\"del-skill\" class=\"btn btn-dark\" onclick=\"return Delete(".$skill_row['sId'].">Delete</button>
+                      </div>";
+            }
+          }
+          ?>
+          <div class="skill-row">
+            <p>Add Skill</p>
+            <form>
+              <?php
+              $new_skill_sql="SELECT sTitle, sId FROM skills WHERE sId NOT IN (SELECT sId FROM userSkills WHERE uId='".$_SESSION['uId']."');";
+              $new_skill_result=$conn->query($new_skill_sql);
+              if ($conn && ($new_skill_result->num_rows>0)) {
+                  while ($new_skill_row=$new_skill_result->fetch_assoc()) {
+                    echo "input type=\"radio\" name=\"newSkill\">".$new_skill_row['sTitle']."</input>";
+                }
+              }
+              ?>
+              <input type="submit" name="submit">
+            </form>
+          </div>
+    			<button type="submit" class="btn btn-primary" style="background-color: green">Update</button>
+  		  </div> 
+      </div>
+      <div class="col-md-4">
+  		  <div class="form-group">
+    			<label for="comment">Qualifications:</label>
+    			<textarea class="form-control" rows="5" id="comment"></textarea>
+    			<button type="submit" class="btn btn-primary" style="background-color: green">Update</button>
+  		  </div> 
+  	  </div>
+      <div class="col-md-4">
+    		<div class="form-group">
+    			<label for="comment">Employment History:</label>
+    			<textarea class="form-control" rows="5" id="comment"></textarea>
+    			<button type="submit" class="btn btn-primary" style="background-color: green">Update</button>
+    		</div> 
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
--- Profile.php
+++ Profile.php
@@ -2,13 +2,33 @@
     session_start();
  
     include 'includes/db.php';
+    if(isset($_GET['uId'])) {
+        $profile_firstname_sql="SELECT uFirstName FROM users where uId = '".$_GET['uId']."';";
+        $profile_firstname_result=$conn->query($profile_firstname_sql);
+        if ($conn && ($profile_firstname_result->num_rows>0)) {
+            while ($profile_firstname_row=$profile_firstname_result->fetch_assoc()) {
+                $profile_firstname = $profile_firstname_row['uFirstName'];
+            }
+        }
+        $profile_lastname_sql="SELECT uLastName FROM users where uId = '".$_GET['uId']."';";
+        $profile_lastname_result=$conn->query($profile_lastname_sql);
+        if ($conn && ($profile_lastname_result->num_rows>0)) {
+            while ($profile_lastname_row=$profile_lastname_result->fetch_assoc()) {
+                $profile_lastname = $profile_lastname_row['uLastName'];
+            }
+        }
+    }
 ?>
  
 <html>
 <head>
     <title>
         <?php
+        if(isset($_GET['uId'])) {
+            echo $profile_firstname." ".$profile_lastname;
+        } else {
             echo $_SESSION['uFirst']." ".$_SESSION['uLast'];
+        }
         ?>
     </title>
     <meta charset="utf-8">
@@ -40,9 +60,16 @@
         <div class="name">
             <h2>
                 <?php
-                    echo $_SESSION['uFirst']." ".$_SESSION['uLast'];
+                    if(isset($_GET['uId'])) {
+                        echo $profile_firstname." ".$profile_lastname;
+                    } else {
+                        echo $_SESSION['uFirst']." ".$_SESSION['uLast'];
+                    }
                 ?>
             </h2>
+        </div>
+        <div class="edit-or-add-friend">
+            
         </div>
         <div class="container">
             <div class="col-sm-8">
@@ -50,10 +77,10 @@
                     <div class="tabbable-line">
                         <ul class="nav nav-tabs">
                             <li class="nav-item active">
-                                <a href="#tab_friends" class="nav-link" role="tab" data-toggle="tab">Friends</a>
-                            </li>
-                            <li class="nav-item active">
                                 <a href="#tab_details" class="nav-link" role="tab" data-toggle="tab">Details</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#tab_friends" class="nav-link" role="tab" data-toggle="tab">Friends</a>
                             </li>
                             <li class="nav-item">
                                 <a href="#tab_contact" class="nav-link" role="tab" data-toggle="tab">Contact info</a>
@@ -67,24 +94,14 @@
                         </ul>
                         <div class="tab-content">
                             <div role="tabpanel" class="tab-pane active" id="tab_details">
-                                <?php
-                                $friend_sql="SELECT uFirstName, uLastName FROM users u, friends f WHERE u.uId=f.uid1 AND f.uid2 = '".$_SESSION['uId']."';";
-                                $friend_result=$conn->query($friend_sql);
-                                if ($conn && ($friend_result->num_rows>0)) {
-                                    while ($friend_row=$friend_result->fetch_assoc()) {
-                                        echo
-                                        "<div>
-                                            <img src=\"http://via.placeholder.com/80x80px\">
-                                            <p>".$friend_row['uFirstName']." ".$friend_row['uLastName']."</p>
-                                        </div>";
-                                    }
-                                }
-                                ?>
-                            </div>
-                            <div role="tabpanel" class="tab-pane active" id="tab_details">
                                 <p>
                                     <?php
-                                    $bio_sql="SELECT uBio FROM users where uId = '".$_SESSION['uId']."';";
+                                    $bio_sql="";
+                                    if(isset($_GET['uId'])) {
+                                        $bio_sql="SELECT uBio FROM users where uId = '".$_GET['uId']."';";
+                                    } else {
+                                        $bio_sql="SELECT uBio FROM users where uId = '".$_SESSION['uId']."';";
+                                    }
                                     $bio_result=$conn->query($bio_sql);
                                     if ($conn && ($bio_result->num_rows>0)) {
                                         while ($bio_row=$bio_result->fetch_assoc()) {
@@ -94,11 +111,36 @@
                                     ?>
                                 </p>
                             </div>
+                            <div role="tabpanel" class="tab-pane active" id="tab_details">
+                                <?php
+                                $friend_sql="";
+                                if(isset($_GET['uId'])) {
+                                    $friend_sql="SELECT uId, uFirstName, uLastName FROM users u, friends f WHERE u\.uId=f\.uid1 AND f\.uid2 = '".$_GET['uId']."';";
+                                } else {
+                                    $friend_sql="SELECT uId, uFirstName, uLastName FROM users u, friends f WHERE u\.uId=f\.uid1 AND f\.uid2 = '".$_SESSION['uId']."';";
+                                }
+                                $friend_result=$conn->query($friend_sql);
+                                if ($conn && ($friend_result->num_rows>0)) {
+                                    while ($friend_row=$friend_result->fetch_assoc()) {
+                                        echo
+                                        "<div>
+                                            <img src=\"http://via.placeholder.com/80x80px\">
+                                            <a href=\"Profile.php?uId=".$friend_row['uId']."\">".$friend_row['uFirstName']." ".$friend_row['uLastName']."</p>
+                                        </div>";
+                                    }
+                                }
+                                ?>
+                            </div>
                             <div role="tabpanel" class="tab-pane" id="tab_contact">
                                 <p><b>Address</b></p>
                                 <p>
                                     <?php
-                                    $contact_sql="SELECT uAddress, uContactNo FROM users WHERE uId = '".$_SESSION['uId']."';";
+                                    $contact_sql="";
+                                    if(isset($_GET['uId'])) {
+                                        $contact_sql="SELECT uAddress, uContactNo FROM users WHERE uId = '".$_GET['uId']."';";
+                                    } else {
+                                        $contact_sql="SELECT uAddress, uContactNo FROM users WHERE uId = '".$_SESSION['uId']."';";
+                                    }
                                     $contact_result=$conn->query($contact_sql);
                                     if ($conn && ($contact_result->num_rows>0)) {
                                         while ($contact_row=$contact_result->fetch_assoc()) {
@@ -120,7 +162,12 @@
                             </div>
                             <div role=\"tabpanel\" class=\"tab-pane\" id=\"tab_skills\">
                                 <?php
-                                $skill_sql="SELECT sTitle FROM skills s, userSkills u WHERE u.usId=s.sId AND u.uId= '".$_SESSION['uId']."';";
+                                $skill_sql="";
+                                if(isset($_GET['uId'])) {
+                                    $skill_sql="SELECT sTitle FROM skills s, userSkills u WHERE u.sId=s.sId AND u.uId= '".$_GET['uId']."';";
+                                } else {
+                                    $skill_sql="SELECT sTitle FROM skills s, userSkills u WHERE u.sId=s.sId AND u.uId= '".$_SESSION['uId']."';";
+                                }
                                 $skill_result=$conn->query($skill_sql);
                                 if ($conn && ($skill_result->num_rows>0)) {
                                     while ($skill_row=$skill_result->fetch_assoc()) {
