133dd0fb02fee836dd2c4963410ce59d3b7e7d18
John Long <john.chris.long@gmail.com>
2018-04-04 14:14:31 +0100

    Added checks for dropdown values so output of search will look different based on what you are searching for

--- includes/searchResults.php
+++ includes/searchResults.php
@@ -1,20 +1,40 @@
 <?php
     include 'navbar_template.html';
-    include 'db.php';
-
-
+    
       if(isset($_POST["search"]) && isset($_POST["dropDown"])){
+            include 'db.php';
+
             $searchInput = mysqli_real_escape_string($conn, $_POST["data"]);
             $dropDownValue = mysqli_real_escape_string($conn, $_POST["dropDown"]);
 
-            $query = mysqli_query($conn, "SELECT uFirstName FROM" . " $dropDownValue " . 
+            if ($dropDownValue == "Users") {
+                $query = mysqli_query($conn, "SELECT uFirstName FROM users " . 
                                                     "WHERE uFirstName LIKE" . "'%"."$searchInput"."%';");
 
-            while($row = mysqli_fetch_row($query))
-            {
-                printf("<ul><li>%s\n</li></ul>", $row[0]);
-            }
-        }
+                while($row = mysqli_fetch_row($query))
+                {
+                    printf("<ul><li>%s\n</li></ul>", $row[0]);
+                }
 
-    mysqli_close($conn);
+            }else if ($dropDownValue == "Hotels") {
+                $query = mysqli_query($conn, "SELECT hName FROM hotels" . 
+                                                    "WHERE hName LIKE" . "'%"."$searchInput"."%';");
+                while($row = mysqli_fetch_row($query))
+                {
+                    printf("<ul><li>%s\n</li></ul>", $row[0]);
+                }
+
+            }else if($dropDownValue == "Vacancies") {
+                 $query = mysqli_query($conn, "SELECT vName FROM vacancies " . 
+                                                    "WHERE vName LIKE" . "'%"."$searchInput"."%';");
+
+                while($row = mysqli_fetch_row($query))
+                {
+                    printf("<ul><li>%s\n</li></ul>", $row[0]);
+                }
+            }
+           
+             echo "$dropDownValue";
+             mysqli_close($conn);
+        }
 ?>
