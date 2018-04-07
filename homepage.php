a4334ccd5596b79a81f4ebe6b6240de25bc4200c
Joseph Tobin <16163842@studentmail.ul.ie>
2018-04-08 00:06:24 +0100

    Simple showing of vacancies added

--- homepage.php
+++ homepage.php
@@ -58,15 +58,17 @@
                 </div>
             </div>
             <div class = "col-lg-8">
-                <div class = "d-flex flex-column p-2">
-                    <p class = "p-2">Vacancy</p>
-                    <p class = "p-2">Vacancy</p>
-                    <p class = "p-2">Vacancy</p>
-                    <p class = "p-2">Vacancy</p>
-                    <p class = "p-2">Vacancy</p>
+                <div id = "vacancies" class = "d-flex flex-column p-2">
+                    <?php 
+                        include 'includes/db.php';
+                        $sql = "SELECT * FROM vacancies;";
+                        $result = mysqli_query($conn, $sql);
+                        while ($row = mysqli_fetch_assoc($result)) { ?>
+                            <div class = "p-2"><?php echo $row["vName"]; echo " "; echo $row['vDescr']; ?></div>
+                    <?php } ?>
                 </div>
             </div>
-            <div class = "col-lg-2">
+            <div class = "col-xs-2">
                 <div class = "d-flex flex-column p-2">
                     <div class = "d-flex flex-column">
                         <div class = "d-flex flex-column p-2">
@@ -113,5 +115,6 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
   </body>
 </html>
