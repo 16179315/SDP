3f1c40a8f152e97dadee8f61236205e8c18bfdc0
Joseph Tobin <16163842@studentmail.ul.ie>
2018-04-07 17:06:16 +0100

    Forgot password now works for hotels

--- includes/forgotPasswordCheck.php
+++ includes/forgotPasswordCheck.php
@@ -4,19 +4,32 @@
         include 'db.php';
 
         $email = mysqli_real_escape_string($conn, $_POST['email']);
-        $sql = "SELECT * FROM users WHERE uEmail = '$email';";
-		$result = mysqli_query($conn, $sql);
-		$resultCheck = mysqli_num_rows($result);
-		if ($resultCheck < 1) {
+        $sql1 = "SELECT * FROM users WHERE uEmail = '$email';";
+        $sql2 = "SELECT * FROM hotels WHERE email = '$email';";
+        $result1 = mysqli_query($conn, $sql1);
+        $result2 = mysqli_query($conn, $sql2);
+        $resultCheck1 = mysqli_num_rows($result1);
+        $resultCheck2 = mysqli_num_rows($result2);
+		if ($resultCheck1 < 1 && $resultCheck2 < 1) {
             $_SESSION['noUserExists'] = true;
             header("Location: ../forgotPassword.php");
 		} else {
+            if ($resultCheck1 > 0) {
+                $token = bin2hex(openssl_random_pseudo_bytes(128));
+                $conn->query("UPDATE users SET token='$token' WHERE uEmail='$email'");
+                // At the moment this just sets the URL to the reset password link. This will hopefully be emailed in the future if possible.
+                header("Location: ../includes/resetPassword.php?email=$email&token=$token");
+                //header("Location: ../?userCheck");
+                exit();
+            }
+            else {
             $token = bin2hex(openssl_random_pseudo_bytes(128));
-            $conn->query("UPDATE users SET token='$token' WHERE uEmail='$email'");
+            $conn->query("UPDATE hotels SET token='$token' WHERE email='$email'");
             // At the moment this just sets the URL to the reset password link. This will hopefully be emailed in the future if possible.
             header("Location: ../includes/resetPassword.php?email=$email&token=$token");
+            //header("Location: ../?hotelCheck");
             exit();
+            }
         }
-
     }
 ?>
--- includes/resetPassword.php
+++ includes/resetPassword.php
@@ -5,25 +5,39 @@
         $email = mysqli_real_escape_string($conn, $_GET["email"]);
         $token = mysqli_real_escape_string($conn, $_GET["token"]);
 
-        $sql = "SELECT uId FROM users WHERE uEmail = '$email' AND token='$token';";
-		$result = mysqli_query($conn, $sql);
-		$resultCheck = mysqli_num_rows($result);
-		if ($resultCheck < 1) {
-			header("Location: ../?invalidTokenOrEmail");
+        $sql1 = "SELECT uId FROM users WHERE uEmail = '$email' AND token='$token';";
+        $result1 = mysqli_query($conn, $sql1);
+        $resultCheck1 = mysqli_num_rows($result1);
+        $sql2 = "SELECT hId FROM hotels WHERE email = '$email' AND token='$token';";
+        $result2 = mysqli_query($conn, $sql2);
+        $resultCheck2 = mysqli_num_rows($result2);
+		if ($resultCheck1 < 1 && $resultCheck2 < 1) {
+			header("Location: ../?invalidTokenOrEmail?email=$email&token=$token");
             exit();
 		} else {
-            $a = "qwertyuiopasdfghjklzxcvbnm";
-            $a = str_shuffle($a);
-            $a = substr($a, 0, 5);
-            $password = sha1($a);
-            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-            $sql = "UPDATE users SET uPassword='$hashedPassword', token='' WHERE uEmail='$email';";
-            mysqli_query($conn, $sql);
-
-            echo "Your new password is: $password";
+            if ($resultCheck1 > 0) {
+                $a = "qwertyuiopasdfghjklzxcvbnm";
+                $a = str_shuffle($a);
+                $a = substr($a, 0, 5);
+                $password = sha1($a);
+                $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    
+                $sql = "UPDATE users SET uPassword='$hashedPassword', token='' WHERE uEmail='$email';";
+                mysqli_query($conn, $sql);
+            }
+            else {
+                $a = "qwertyuiopasdfghjklzxcvbnm";
+                $a = str_shuffle($a);
+                $a = substr($a, 0, 5);
+                $password = sha1($a);
+                $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    
+                $sql = "UPDATE hotels SET password='$hashedPassword', token='' WHERE email='$email';";
+                mysqli_query($conn, $sql);
+    
+                echo "Your new password is: $password";
+            }
         }
-
     }
 
 ?>
