598922dffdea554082d3ea3455f9c6a1ff08e89b
John Long <john.chris.long@gmail.com>
2018-04-12 15:28:41 +0100

    Added tables for search results

--- searchResults.php
+++ searchResults.php
@@ -10,18 +10,46 @@
             if(strcmp($dropDownValue, "users") == 0) {
                 $query = mysqli_query($conn, "SELECT uFirstName FROM users " . 
                                                     "WHERE uFirstName LIKE" . "'%"."$searchInput"."%';");
+                var_dump($query);
 
                 while($row = mysqli_fetch_row($query))
-                {
-                    printf("<ul><li>%s\n</li></ul>", $row[0]);
+                {   
+                    printf("<table>
+                              <tr>
+                                <th>User Name</th>
+                                <th>Email</th>
+                                <th>Contact No</th>
+                                <th>Address</th>
+                              </tr>
+                              <tr>
+                                <td>%s</td>
+                                <td>email</td>
+                                <td>contact no</td>
+                                <td>address</td>
+                              </tr>
+                            </table></br>", $row[0]);
                 }
 
             }elseif (strcmp($dropDownValue, "hotels") == 0) {
                 $query = mysqli_query($conn, "SELECT hName FROM hotels " . 
                                                     "WHERE hName LIKE" . "'%"."$searchInput"."%';");
+
                 while($row = mysqli_fetch_row($query))
                 {
-                    printf("<ul><li>%s\n</li></ul>", $row[0]);
+                    printf("<table>
+                              <tr>
+                                <th>Hotel Name</th>
+                                <th>Address</th>
+                                <th>Email</th>
+                                <th>Telephone</th>
+                              </tr>
+                              <tr>
+                                <td>%s</td>
+                                <td>address</td>
+                                <td>email</td>
+                                <td>telephone</td>
+                              </tr>
+                            </table></br>", $row[0]);
                 }
 
             }elseif(strcmp($dropDownValue, "vacancies") == 0) {
@@ -30,7 +58,22 @@
 
                 while($row = mysqli_fetch_row($query))
                 {
-                    printf("<ul><li>%s\n</li></ul>", $row[0]);
+                    printf("<table>
+                              <tr>
+                                <th>Vacancy Type</th>
+                                <th>Description</th>
+                                <th>Hotel</th>
+                                <th>Status</th>
+                                <th>Salary</th>
+                              </tr>
+                              <tr>
+                                <td>%s</td>
+                                <td>description</td>
+                                <td>hotel</td>
+                                <td>status</td>
+                                <td>salary</td>
+                              </tr>
+                            </table></br>", $row[0]);
                 }
             }
             
