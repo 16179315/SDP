7df5be65699cc10fffe70ba258120c3fd8907b2e
Joseph Tobin <16163842@studentmail.ul.ie>
2018-04-19 21:55:25 +0100

    login fixed

--- includes/login.php
+++ includes/login.php
@@ -42,7 +42,7 @@ if (isset($_POST['login'])) {
 				header("Location: ../?loginUserPassword=error");
 				exit();
 			} elseif ($hashedPasswordCheck == true) {
-				$sql0 = "SELECT * FROM bannedUsers WHERE uId = ".$row[uId]."";
+				$sql0 = "SELECT * FROM bannedUsers WHERE uId = ".$row['uId']."";
 				$result0 = mysqli_query($conn, $sql0);
 				$row0 = mysqli_fetch_assoc($result0);
 				$currDate = date("Y-m-d");
@@ -52,18 +52,18 @@ if (isset($_POST['login'])) {
 					exit();
 				}
 				else {
-				$sql10 = "SELECT uAdmin FROM users WHERE uId = ".$row[uId]."";
+				$sql10 = "SELECT uAdmin FROM users WHERE uId = ".$row['uId']."";
 				$result10 = mysqli_query($conn, $sql10);
 				$row10 = mysqli_fetch_assoc($result10);
-				if ($row10[uAdmin] == 1) {
+				if ($row10['uAdmin'] == 1) {
 					$_SESSION['adminLoggedIn'] = true;
 					$url = "../admin.php";
 					header("Location: ".$url);
 					exit();
 				}
 				else {
-					$_SESSION['uId'] = $row[uId];
-					$_SESSION['uFirst'] = $row[uFirstName];
+					$_SESSION['uId'] = $row['uId'];
+					$_SESSION['uFirst'] = $row['uFirstName'];
 					$_SESSION['userLoggedIn'] = true;
 					$url = "../homepage.php";
 					header("Location: ".$url);
