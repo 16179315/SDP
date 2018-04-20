a396158bf5f5ab486bb1e088e726eb2f1627f356
Patrick O'Hora <patrick55529@gmail.com>
2018-04-20 01:23:42 +0100

    php for admin

---
+++ includes/editBasicHotel.php
@@ -0,0 +1,68 @@
+<?php 
+
+
+	session_start();
+		$success = true;
+	include 'db.php';
+	$hId = $_POST['hId'];
+	if(!($_POST['inputEmail'] == "")) {
+		$email = $_POST['inputEmail'];
+		$sql = "UPDATE hotels SET email = '$email' where hId = '$hId'";
+		$retval = mysqli_query($conn,$sql);
+		if(! $retval )
+		{
+		  die('Could not update data:' . mysql_error());
+		  		  $url = "../adminEditHotel.php?hId=".$hId;
+		  header($url);
+		  exit();
+		}
+	}
+	if(!($_POST['inputContactNumber'] == "")) {
+		$contactNo = $_POST['inputContactNumber'];
+		$sql = "UPDATE hotels SET contactNo = '$contactNo' where hId = '$hId'";
+		$retval = mysqli_query($conn,$sql);
+		if(! $retval )
+		{
+		  die('Could not update data:' . mysql_error());
+		  $url = "../adminEditHotel.php?hId=".$hId;
+		  header("Location: ".$url);
+		  exit();
+		}
+	}
+	if(!($_POST['inputAddress'] == "")) {
+		$address = $_POST['inputAddress'];
+		$sql = "UPDATE hotels SET address = '$address' where hId = '$hId'";
+		$retval = mysqli_query($conn,$sql);
+		if(! $retval )
+		{
+		  die('Could not update data: ' . mysql_error());
+		  		  $url = "../adminEditHotel.php?hId=".$hId;
+		  header("Location: ".$url);
+		  exit();
+		}
+	}
+	if(!($_POST['inputDescription'] == "")) {
+		$descr = $_POST['inputDescription'];
+		echo $descr;
+		$sql = "UPDATE hotels SET descr = '$descr' where $hId = '$hId'";
+		$retval = mysqli_query($conn,$sql );
+		if(! $retval )
+		{
+		  die('Could not update data:' . mysql_error());
+		  		  $url = "../adminEditHotel.php?hId=".$hId;
+		  header("Location: ".$url);
+		  exit();
+		}
+	}
+	
+	if($success) {
+		$_SESSION['success'] = true;
+		$url = "../adminEditHotel.php?hId=".$hId;
+		header("Location: ".$url);
+		exit();
+	}
+	?>
+	
+	
+	
+	
